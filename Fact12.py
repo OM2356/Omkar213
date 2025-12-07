e651d009e55bc82561cf75fc9f45e1c3990d410a
Omkar . R . Sathe <omkarsathe3103@gmail.com>
2025-12-07 21:38:41 +0530

    Add files via upload

---
+++ Fact12.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Function call
+num = 5
+print("Factorial of", num, "is", factorial(num))
---
+++ math.py
@@ -0,0 +1,7 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square Root:", math.sqrt(num))
+print("Natural Logarithm:", math.log(num))
+print("Sine:", math.sin(num))
